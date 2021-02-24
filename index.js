38486ac29740763d354e77cc271d7270ceb5c66b
kate <katy.madden99@gmail.com>
2021-02-24 15:13:48 +0000

    Added new row feature to adapt styling required around bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const coreHTML = require("./generateHTML");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+var employees = 1;
 
 const mngQuestions = ["What's the manager's name?", "What's the manager's ID number?", "What's the manager's email?", "What's the manager's office phone number", "Who else do you want to add?"];
 const engQuestions = ["What's the engineer's name?", "What's the engineer's ID number?", "What's the engineer's email?", "What's the engineer's GitHub url?", "Who else do you want to add?"];
@@ -47,6 +50,7 @@ function init() {
                 return internQ();
             }
             else {
+                fs.appendFileSync('./dist/Team-Rota.html', endHTML());
                 return console.log("Rota generated!");
             }
         });
@@ -83,13 +87,16 @@ engineerQ = () => {
         }
     ])
     .then((data) => {
-        if (data.mngOthers === "Add an Engineer") {
+        fs.appendFileSync('./dist/Team-Rota.html', addEng(data));
+
+        if (data.engOthers === "Add an Engineer") {
             return engineerQ();
         } 
-        else if (data.mngOthers === "Add an Intern") {
+        else if (data.engOthers === "Add an Intern") {
             return internQ();
         }
         else {
+            fs.appendFileSync('./dist/Team-Rota.html', endHTML());
             return console.log("Rota generated!");
         }
     });
@@ -122,17 +129,20 @@ internQ = () => {
             type: 'list',
             message: intQuestions[4],
             name: 'intOthers',
-            choices: ['Add a Manager', 'Add an Engineer', 'Add an Intern', 'I do not want to add anyone else']
+            choices: ['Add an Engineer', 'Add an Intern', 'I do not want to add anyone else']
         }
     ])
     .then((data) => {
-        if (data.mngOthers === "Add an Engineer") {
+        fs.appendFileSync('./dist/Team-Rota.html', addInt(data));
+
+        if (data.intOthers === "Add an Engineer") {
             return engineerQ();
         } 
-        else if (data.mngOthers === "Add an Intern") {
+        else if (data.intOthers === "Add an Intern") {
             return internQ();
         }
         else {
+            fs.appendFileSync('./dist/Team-Rota.html', endHTML());
             return console.log("Rota generated!");
         }
     });
@@ -141,6 +151,59 @@ internQ = () => {
 function writeToFile(data) {
     fs.writeFileSync("./dist/Team-Rota.html", coreHTML(data), (err) =>
       err ? console.error(err) : console.log('Manager added!'));
+}
+
+function addEng(data) {
+    let E = new Engineer(data.engName, data.engID, data.engEmail, data.engGithub);
+    newRow();
+    employees++;
+    return `<div class="card bg-light mb-3 col-4 mx-3" style="max-width: 18rem;">
+            <div class="card-header text-white bg-primary">
+            <h4>${E.name}</h4>
+            <h5 class="card-title text-white"><i class="fas fa-glasses"></i> Engineer</h5>    
+            </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${E.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${E.email}">${E.email}</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${E.github}" target="_blank">${E.github}</a></li>
+                  </ul>
+            </div>
+          </div>`
+}
+
+function addInt(data) {
+    let I = new Intern(data.intName, data.intID, data.intEmail, data.school);
+    newRow();
+    employees++;
+    return `<div class="card bg-light mb-3 col-4 mx-3" style="max-width: 18rem;">
+            <div class="card-header text-white bg-primary">
+            <h4>${I.name}</h4>
+            <h5 class="card-title text-white"><i class="fas fa-user-graduate"></i> Intern</h5>    
+            </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${I.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${I.email}">${I.email}</a></li>
+                    <li class="list-group-item">School: ${I.school}</li>
+                  </ul>
+            </div>
+          </div>`
+}
+
+function newRow() {
+    if(employees > 3 && employees % 3 == 0) {
+    fs.appendFileSync('./dist/Team-Rota.html', `</div>
+    <div class="row">
+    `);
+}
+}
+
+function endHTML() {
+    return `</div>
+    </article>
+    </body>
+    </html>`
 }
 
 init();
